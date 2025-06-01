9fb12b811dc1fcc539712f72d086cc608d3c7b09
Faizan Hemotra <77664620+fzhem@users.noreply.github.com>
2025-06-01 22:45:31 +0100

    Update octmarkermainwindow.cpp

--- src/windows/octmarkermainwindow.cpp
+++ src/windows/octmarkermainwindow.cpp
@@ -438,13 +438,13 @@ void OCTMarkerMainWindow::setupMenu()
 	QAction* previousOctScan = new QAction(this);
 	previousOctScan->setText(tr("previous octScan"));
 	previousOctScan->setIcon(QIcon::fromTheme("go-previous", QIcon(":/icons/tango/actions/go-previous.svgz")));
-	previousOctScan->setShortcut(Qt::CTRL + Qt::LeftArrow);
+	previousOctScan->setShortcut(Qt::CTRL | Qt::Key_Left);
 	connect(previousOctScan, &QAction::triggered, &(OctFilesModel::getInstance()), &OctFilesModel::loadPreviousFile);
 
 	QAction* nextOctScan = new QAction(this);
 	nextOctScan->setText(tr("next octScan"));
 	nextOctScan->setIcon(QIcon::fromTheme("go-next", QIcon(":/icons/tango/actions/go-next.svgz")));
-	nextOctScan->setShortcut(Qt::CTRL + Qt::RightArrow);
+	nextOctScan->setShortcut(Qt::CTRL | Qt::Key_Right);
 	connect(nextOctScan, &QAction::triggered, &(OctFilesModel::getInstance()), &OctFilesModel::loadNextFile);
 
 
